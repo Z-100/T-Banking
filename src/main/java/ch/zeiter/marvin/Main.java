0a36964afd2570b8a17a8f70ef18032686a38f6c
marvin <marvin131t@gmail.com>
2021-10-05 10:17:57 +0200

    Added JavaDoc to main method

--- src/main/java/ch/zeiter/marvin/Main.java
+++ src/main/java/ch/zeiter/marvin/Main.java
@@ -10,6 +10,10 @@ import ch.zeiter.marvin.io.Cli;
 
 public class Main {
 
+    /**
+     * Method used to start the program
+     * @param args Starting argument
+     */
     public static void main(String[] args) {
         String name = "TBZ E-Banking";
 
