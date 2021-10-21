e696b67f9b71f7db383a8664814cf3fbfb4537cb
marvin.zeiter <marvin.zieter@suisa.ch>
2021-10-21 08:48:38 +0200

    Added streams and functional programming

--- src/main/java/ch/zeiter/marvin/functions/CreateAccount.java
+++ src/main/java/ch/zeiter/marvin/functions/CreateAccount.java
@@ -3,25 +3,34 @@ package ch.zeiter.marvin.functions;
 import ch.zeiter.marvin.Blueprints.Account;
 import ch.zeiter.marvin.other.AccountPropertyGenerator;
 import lombok.Getter;
-import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.function.Predicate;
 
 public class CreateAccount {
 
     @Getter
     private AccountPropertyGenerator acg = new AccountPropertyGenerator();
 
+    /**
+     * Method used to create a new user account
+     *
+     * @param inputPassword The user chosen password
+     * @return The newly created account
+     */
     public Account createAccount(String inputPassword) {
-
         Json json = new Json();
 
+        // * Could be done without the predicate initialization, but I want to show off my knowledge
+        Predicate<Account> ibanPredicate = acc -> acc.getIban().equals(acg.getIBan());
+        Predicate<Account> uuidPredicate = acc -> acc.getUuid().equals(acg.getUuid());
+
         try {
-            for (Account acc : json.getFromJson("Accounts/accounts.json")) {
-                if (acc.getIban().equals(acg.getIBan()) || acc.getUuid().equals(acg.getUuid()))
-                    acg = new AccountPropertyGenerator();
-            }
-        } catch (IOException | ParseException e) {
+            json.getFromJson("Accounts/accounts.json").stream()
+                    .filter(ibanPredicate)
+                    .filter(uuidPredicate)
+                    .forEach(acc -> acg = new AccountPropertyGenerator());
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
--- src/main/java/ch/zeiter/marvin/functions/TransactionHandler.java
+++ src/main/java/ch/zeiter/marvin/functions/TransactionHandler.java
@@ -6,6 +6,7 @@ import ch.zeiter.marvin.other.UserSession;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.function.Function;
 
 public class TransactionHandler {
 
@@ -29,9 +30,21 @@ public class TransactionHandler {
      * @return Useful information for the user
      */
     public String newTransaction(double amountOfMoney) {
-        if (verifyTransaction(new Transaction(amountOfMoney, account.getBalance(), 0))) {
+        Function<Transaction, Boolean> verifyTransaction = transaction -> {
+            this.oldBalance = transaction.getBalanceTwo();
+            this.account.setBalance(
+                    transaction.getBalanceTwo() + transaction.getBalanceOne());
+
+            return account.getBalance() - transaction.getBalanceOne() == this.oldBalance;
+        };
+
+        Boolean transactionSuccess = verifyTransaction.apply(
+                new Transaction(amountOfMoney, account.getBalance(), 0));
+
+
+        if (transactionSuccess) {
             try {
-                json.saveToJson(this.account, "Accounts/accounts.json");
+                json.saveToJson(this.account, "Accounts/accounts.json", false);
                 return "Transaction successful";
             } catch (IOException | ParseException e) {
                 e.printStackTrace();
@@ -41,19 +54,5 @@ public class TransactionHandler {
             account.setBalance(this.oldBalance);
             return "Something went wrong";
         }
-    }
-
-    /**
-     * Method used to verify the proper execution of a transaction
-     *
-     * @param transaction The to be checked transaction
-     * @return true if the transaction was successful
-     */
-    private boolean verifyTransaction(Transaction transaction) {
-        this.oldBalance = transaction.getBalanceTwo();
-        account.setBalance(
-                transaction.getBalanceTwo() + transaction.getBalanceOne());
-
-        return account.getBalance() - transaction.getBalanceOne() == this.oldBalance;
     }
 }
