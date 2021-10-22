bd3382773c2df6a0a5404c5005d0621d2699781d
marvin.zeiter <marvin.zieter@suisa.ch>
2021-10-22 16:23:30 +0200

    Added JavaFX Support

--- src/main/java/ch/zeiter/marvin/Main.java
+++ src/main/java/ch/zeiter/marvin/Main.java
@@ -6,18 +6,49 @@
 
 package ch.zeiter.marvin;
 
-import ch.zeiter.marvin.io.Cli;
+import ch.zeiter.marvin.controller.LoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+import java.io.IOException;
+
+public class Main extends Application {
+
 
     /**
      * Method used to start the program
      * @param args Starting argument
      */
     public static void main(String[] args) {
-        String name = "TBZ E-Banking";
+//        String name = "TBZ E-Banking";
+//
+//        Cli cli = new Cli();
+//        cli.init(name);
+        launch(args);
+    }
 
-        Cli cli = new Cli();
-        cli.init(name);
+    @Override
+    public void start(Stage primaryStage) {
+
+        loginpage(primaryStage);
+    }
+
+    public void loginpage(Stage primaryStage ) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Screens/fxml/Login.fxml"));
+            Parent root = fxmlLoader.load();
+            LoginController controller = fxmlLoader.getController();
+            controller.init(primaryStage);
+
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setResizable(false);
+            primaryStage.setTitle("TBZ E-Banking");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/ch/zeiter/marvin/controller/LoginController.java
@@ -0,0 +1,49 @@
+package ch.zeiter.marvin.controller;
+
+import ch.zeiter.marvin.functions.Login;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class LoginController {
+
+	@FXML
+	private TextField usernameField;
+	@FXML
+	private PasswordField passwordField;
+	@FXML
+	private Button loginBtn;
+	@FXML
+	private Label registerbtn;
+
+	public void init(Stage primaryStage) {
+
+		AtomicInteger i = new AtomicInteger();
+
+		usernameField.requestFocus();
+
+		loginBtn.setOnAction(actionEvent -> {
+			if (i.get() >= 5)
+				Platform.exit();
+
+			loginCheck(usernameField.getText(), passwordField.getText());
+
+			i.getAndIncrement();
+		});
+
+		registerbtn.setOnMouseClicked(mouseEvent -> {
+			//TODO add logic to change page
+		});
+	}
+
+	private void loginCheck(String iBan, String password) {
+		Login login = new Login(null);
+		login.loginCheck(iBan, password);
+	}
+}
